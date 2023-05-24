8093316fb84649ea769b62cb0bdaeca1726c2a46
kimnayu98 <rlaskdbwkd@naver.com>
2023-05-24 16:20:49 +0900

    feat:test

--- app/src/main/java/com/example/cinemaisland/MainActivity.kt
+++ app/src/main/java/com/example/cinemaisland/MainActivity.kt
@@ -1,11 +1,29 @@
-package com.example.cinemaisland
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var titleTextView: TextView
+    private lateinit var contentTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        titleTextView = findViewById(R.id.titleTextView)
+        contentTextView = findViewById(R.id.contentTextView)
+
+        titleTextView.setOnClickListener {
+            val isContentVisible = contentTextView.visibility == View.VISIBLE
+            if (isContentVisible) {
+                contentTextView.visibility = View.GONE
+            } else {
+                contentTextView.visibility = View.VISIBLE
+                val postContent = "공지사항 내용입니다."
+                contentTextView.text = postContent
+            }
+        }
     }
 }
