9a1843ff14b542fe08e70ca5c0339efb20506ce0
kimnayu98 <rlaskdbwkd@naver.com>
2023-05-25 18:07:45 +0900

    feat:notification recyclerview

--- app/src/main/java/com/example/cinemaisland/MainActivity.kt
+++ app/src/main/java/com/example/cinemaisland/MainActivity.kt
@@ -1,10 +1,14 @@
-import android.content.Intent
+package com.example.cinemaisland
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.cinemaisland.R
+import com.example.cinemaisland.notification.NoticeRecyclerAdapter
 import com.example.cinemaisland.notification.NoticeItem
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,37 +16,71 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nRecyclerAdapter: NoticeRecyclerAdapter
     private lateinit var noticeItems: ArrayList<NoticeItem>
 
+    private lateinit var prevButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var pageNumberTextView: TextView
+
+    private var currentPage = 1
+    private val itemsPerPage = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //공지사항 리사이클러 뷰
+        // 공지사항 리사이클러뷰
         nRecyclerView = findViewById(R.id.recyclerView)
 
-        /* initiate adapter */
-        nRecyclerAdapter = NoticeRecyclerAdapter { item ->
-            // 클릭 시 다른 화면으로 이동하고 제목과 본문을 전달
-            val intent = Intent(this, DetailActivity::class.java)
-            intent.putExtra("notice_title", item.notice_title)
-            intent.putExtra("notice_content", item.notice_content)
-            startActivity(intent)
+        // 이전 버튼
+        prevButton = findViewById(R.id.btn_prev)
+        prevButton.setOnClickListener {
+            if (currentPage > 1) {
+                currentPage--
+                updateNoticeList()
+            }
         }
+
+        // 다음 버튼
+        nextButton = findViewById(R.id.btn_next)
+        nextButton.setOnClickListener {
+            val totalPages = getTotalPages()
+            if (currentPage < totalPages) {
+                currentPage++
+                updateNoticeList()
+            }
+        }
+
+        // 페이지 번호 텍스트뷰
+        pageNumberTextView = findViewById(R.id.page_number)
+
+        /* initiate adapter */
+        nRecyclerAdapter = NoticeRecyclerAdapter()
 
         /* initiate recyclerview */
         nRecyclerView.adapter = nRecyclerAdapter
         nRecyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
 
-        //공지사항 간격을 줄이고싶은데 모르겠...도와주십...사 ㅜㅜ
-//        /* set item spacing */
-//        val itemSpacing = resources.getDimensionPixelSize(R.dimen.item_spacing)
-//        nRecyclerView.addItemDecoration(RecyclerView.ItemDecoration(itemSpacing))
-
         /* adapt data */
         noticeItems = ArrayList()
-        for (i in 1..10) {
-            val noticeContent = "본문 내용 $i"
-            noticeItems.add(NoticeItem("id:$i", "$i 번째 제목", "날짜", noticeContent))
+        for (i in 1..30) { // 임의로 30개의 공지사항 아이템을 추가
+            noticeItems.add(NoticeItem("id:${i}", "${i}번째 제목", "날짜", "본문"))
         }
-        nRecyclerAdapter.setNoticeList(noticeItems)
+        updateNoticeList()
+    }
+
+    private fun updateNoticeList() {
+        val start = (currentPage - 1) * itemsPerPage
+        val end = minOf(start + itemsPerPage, noticeItems.size)
+        val sublist = noticeItems.subList(start, end)
+        nRecyclerAdapter.setNoticeList(sublist)
+
+        val totalPages = getTotalPages()
+        pageNumberTextView.text = "페이지 $currentPage / $totalPages"
+
+        prevButton.isEnabled = currentPage > 1
+        nextButton.isEnabled = currentPage < totalPages
+    }
+
+    private fun getTotalPages(): Int {
+        return ceil(noticeItems.size.toDouble() / itemsPerPage).toInt()
     }
 }
