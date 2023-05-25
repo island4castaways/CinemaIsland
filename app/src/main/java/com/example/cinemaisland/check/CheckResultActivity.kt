871c57d91fdf7b6f45b7557e4d004c332d21dd22
monaYR <rufinayk@naver.com>
2023-05-25 12:07:24 +0900

    feat: 응모 결과 검색 구현

--- app/src/main/java/com/example/cinemaisland/check/CheckResultActivity.kt
+++ app/src/main/java/com/example/cinemaisland/check/CheckResultActivity.kt
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.example.cinemaisland.databinding.ActivityCheckResultBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class CheckResultActivity : AppCompatActivity() {
     lateinit var binding: ActivityCheckResultBinding
+    val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    var mode = "default"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,25 +21,51 @@ class CheckResultActivity : AppCompatActivity() {
         //submitBtn 클릭시
         binding.submitBtn.setOnClickListener {
             Log.d("ssum", "submitBtn Clicked")
-            //입력 내용 제출, DB에서 검색
-
-
-            //inputView 보이지 않게 설정
-            binding.inputView.visibility = View.GONE
+            //입력 정보 가져오기
+            val name = binding.inputName.text.toString()
+            val phone = binding.inputPhone.text.toString()
+            val email = binding.inputEmail.text.toString()
+            //DB에서 확인
+            db.collection("winner").get()
+                .addOnSuccessListener { documents ->
+                    for(document in documents) {
+                        Log.d("ssum", "${document.id}, ${document.data}")
+                        if(document.data.getValue("name").toString().equals(name) &&
+                                document.data.getValue("phone").equals(phone) &&
+                                document.data.getValue("email").equals(email)) {
+                            Log.d("ssum", "Winner searched")
+                            mode = "searched"
+                            updateView()
+                        } else {
+                            Log.d("ssum", "Applicant has no prize")
+                            Toast.makeText(this, "입력한 정보로 당첨된 응모 내역이 없습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }.addOnFailureListener {
+                    Log.d("ssum", "Error getting documents, $it")
+                }
 
             //resultView에 검색 정보 출력
 
-
-            //resultView 보이게 설정
-            binding.resultView.visibility = View.VISIBLE
         }
 
         //resetBtn 클릭시
         binding.resetBtn.setOnClickListener {
             Log.d("ssum", "resetBtn Clicked")
+            mode = "default"
+            updateView()
+        }
+    }
+
+    fun updateView() {
+        if(mode == "searched") {
+            //inputView 보이지 않게 설정
+            binding.inputView.visibility = View.GONE
+            //resultView 보이게 설정
+            binding.resultView.visibility = View.VISIBLE
+        } else if(mode == "default") {
             //resultView 보이지 않게 설정
             binding.resultView.visibility = View.GONE
-
             //inputView 보이게 설정
             binding.inputView.visibility = View.VISIBLE
         }
