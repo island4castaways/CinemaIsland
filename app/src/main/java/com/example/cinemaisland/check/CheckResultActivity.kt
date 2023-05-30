aff038cb01bd24b83b0bd935d3eea467f4410344
monaYR <rufinayk@naver.com>
2023-05-30 16:48:37 +0900

    feat: 응모 결과 recyclerView 구현

--- app/src/main/java/com/example/cinemaisland/check/CheckResultActivity.kt
+++ app/src/main/java/com/example/cinemaisland/check/CheckResultActivity.kt
@@ -5,15 +5,26 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cinemaisland.MyApplication
 import com.example.cinemaisland.databinding.ActivityCheckResultBinding
-import com.example.cinemaisland.model.ItemMovie
+import com.example.cinemaisland.model.MovieItem
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
 
 class CheckResultActivity : AppCompatActivity() {
     lateinit var binding: ActivityCheckResultBinding
     val db: FirebaseFirestore = MyApplication.db
     var mode = "default"
+    var movies = ArrayList<String>()
+    var documentId: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,29 +38,41 @@ class CheckResultActivity : AppCompatActivity() {
             val name = binding.inputName.text.toString()
             val phone = binding.inputPhone.text.toString()
             val email = binding.inputEmail.text.toString()
-            //DB에서 확인
-            db.collection("winner").get()
-                .addOnSuccessListener { documents ->
-                    for(document in documents) {
-                        Log.d("ssum", "${document.id}, ${document.data}")
-                        if(document.data.getValue("name").toString().equals(name) &&
-                                document.data.getValue("phone").equals(phone) &&
-                                document.data.getValue("email").equals(email)) {
-                            Log.d("ssum", "Winner searched")
-                            mode = "searched"
+            CoroutineScope(Dispatchers.IO).launch {
+                //DB에서 확인
+                runBlocking {
+                    db.collection("applicant").get()
+                        .addOnCompleteListener { task ->
+                            val result = task.result
+                            for (document in result) {
+                                Log.d("ssum", "${document.id}, ${document.data}")
+                                if (document.data.getValue("name").toString().equals(name) &&
+                                    document.data.getValue("phone").equals(phone) &&
+                                    document.data.getValue("email").equals(email)
+                                ) {
+                                    Log.d("ssum", "Applicant searched")
+                                    documentId = document.id.toString()
+                                    mode = "searched"
 
-                            //resultView에 검색 정보 출력
-                            //db에 array type field data 받아오기
-
+                                    //검색된 응모자 정보에서 당첨된 영화 목록 가져오기
+                                    movies = document["movies"] as ArrayList<String>
+                                    Log.d("ssum", "won movies : ${movies.toString()}")
+                                } else {
+                                    Log.d("ssum", "Applicant has no prize")
+                                    Toast.makeText(
+                                        this@CheckResultActivity,
+                                        "입력한 정보로 당첨된 응모 내역이 없습니다.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                            makeRecyclerView(movies)
                             updateView()
-                        } else {
-                            Log.d("ssum", "Applicant has no prize")
-                            Toast.makeText(this, "입력한 정보로 당첨된 응모 내역이 없습니다.", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                }.addOnFailureListener {
-                    Log.d("ssum", "Error getting documents, $it")
+                        }.addOnFailureListener {
+                            Log.d("ssum", "Error getting applicantList, $it")
+                        }.await()
                 }
+            }
         }
 
         //resetBtn 클릭시
@@ -61,12 +84,12 @@ class CheckResultActivity : AppCompatActivity() {
     }
 
     private fun updateView() {
-        if(mode == "searched") {
+        if (mode == "searched") {
             //inputView 보이지 않게 설정
             binding.inputView.visibility = View.GONE
             //resultView 보이게 설정
             binding.resultView.visibility = View.VISIBLE
-        } else if(mode == "default") {
+        } else if (mode == "default") {
             //resultView 보이지 않게 설정
             binding.resultView.visibility = View.GONE
             //inputView 보이게 설정
@@ -74,7 +97,28 @@ class CheckResultActivity : AppCompatActivity() {
         }
     }
 
-    private fun makeRecyclerView() {
-
+    private fun makeRecyclerView(moviesList: ArrayList<String>) {
+        db.collection("movie").get()
+            .addOnCompleteListener { task ->
+                for (document in task.result) {
+                    for (movie in moviesList) {
+                        val itemList = mutableListOf<MovieItem>()
+                        if (document.data.getValue("title").equals(movie)) {
+                            val item = document.toObject(MovieItem::class.java)
+                            item.id = document.id
+                            itemList.add(item)
+                        }
+                        binding.resultRecyclerView.layoutManager = LinearLayoutManager(this)
+                        binding.resultRecyclerView.adapter = ResultRecyclerAdapter(this, itemList)
+                    }
+                }
+            }.addOnFailureListener {
+                Log.d("ssum", "Error getting moviesList, $it")
+                Toast.makeText(
+                    this,
+                    "서버로부터 데이터를 가져오는데 실패했습니다.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
     }
 }
--- app/src/main/java/com/example/cinemaisland/model/MovieItem.kt
+++ app/src/main/java/com/example/cinemaisland/model/MovieItem.kt
@@ -2,11 +2,14 @@ package com.example.cinemaisland.model
 
 import java.util.Date
 
-class ItemMovie {
+class MovieItem {
+    var id: String? = null
     var title: String? = null
+    var raffleDate: Date? = null
+    var rating: Int = 0
     var genre: String? = null
     var director: String? = null
-    var rating: Int = 0
-    var nation: String? = null
     var schedule: Date? = null
+    var details: String? = null
+    var imageUrl: String? = null
 }
