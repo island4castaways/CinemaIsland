e65309cd7335182d564a9e95caf269a52a671db0
yeonsu10 <jenix1@naver.com>
2023-05-30 11:07:33 +0900

    feat: 툴바로 모든 액티비티에 구현 완료

--- app/src/main/java/com/example/cinemaisland/HomeActivity.kt
+++ app/src/main/java/com/example/cinemaisland/HomeActivity.kt
@@ -2,10 +2,18 @@ package com.example.cinemaisland
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import com.example.cinemaisland.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        val binding = ActivityHomeBinding.inflate(layoutInflater)
+        findViewById<FrameLayout>(R.id.activity_content).addView(binding.root)
+
+    }
+
+    override fun getLayoutResId(): Int {
+        return R.layout.activity_home
     }
 }
--- app/src/main/java/com/example/cinemaisland/MainActivity.kt
+++ app/src/main/java/com/example/cinemaisland/MainActivity.kt
@@ -2,21 +2,26 @@ package com.example.cinemaisland
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cinemaisland.databinding.ActivityMainBinding
 import com.example.cinemaisland.notification.NoticeRecyclerAdapter
 import com.example.cinemaisland.notification.NoticeItem
 import kotlin.math.ceil
 
-class MainActivity : AppCompatActivity() {
-
-
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        findViewById<FrameLayout>(R.id.activity_content).addView(binding.root)
+    }
 
+    override fun getLayoutResId(): Int {
+        return R.layout.activity_main
     }
 }
