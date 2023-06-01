4c25029c2ed150cc7cb96f21860cf3f757a82bcb
hikimhaeun <hikimhaeun@gmail.com>
2023-06-01 17:58:03 +0900

    feat:홈에 탭으로 소개, 주소 탭으로 나눔, 컬러 추가 및 테마 컬러 바꿈..

--- app/src/main/java/com/example/cinemaisland/HomeActivity.kt
+++ app/src/main/java/com/example/cinemaisland/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.example.cinemaisland
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.cinemaisland.databinding.ActivityHomeBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,22 +15,62 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.tabs.TabLayout
 
-class HomeActivity : BaseActivity() , OnMapReadyCallback {
-    //구글맵 연수언니천재
-    var googleMap: GoogleMap? =null
+
+class HomeActivity : BaseActivity(), OnMapReadyCallback {
+    //구글맵 연수언니 천재
+    var googleMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityHomeBinding.inflate(layoutInflater)
         findViewById<FrameLayout>(R.id.activity_content).addView(binding.root)
 
-        (supportFragmentManager.findFragmentById(R.id.mapView) as
-                SupportMapFragment?)!!.getMapAsync(this)    //호출 되면서 아래의 onMapReady()를 호출
+        val tabLayout = binding.tabs
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                val transition = supportFragmentManager.beginTransaction()
+                when (tab?.text) {
+                    "찾아오시는 길" -> transition.replace(R.id.tabContainer, Home_location_Fragment())
+                    "소개" -> transition.replace(R.id.tabContainer, Home_intro_Fragment())
+                }
+                transition.commit()
+            }
+
+            //선택된 탭 버튼을 다시 선택할 때 이벤트
+            override fun onTabSelected(tab: TabLayout.Tab?) {}
+
+            //다른 탭 버튼을 눌러 선택된 탭 버튼이 해제될 때 이벤트
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+        })
+
+        val mapFragment = SupportMapFragment.newInstance()
+        supportFragmentManager.beginTransaction()
+            .add(R.id.tabContainer, mapFragment)
+            .commit()
+        mapFragment.getMapAsync(this)
+//
+//        val mapFragment = supportFragmentManager.findFragmentById(R.id.tabContainer) as SupportMapFragment?
+//        mapFragment?.getMapAsync(this)
 
     }
 
-    //지도뷰 객체 얻기
+    class MyPageAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+        val fragments: List<Fragment>
+
+        init {
+            fragments = listOf(Home_intro_Fragment(), Home_location_Fragment())
+        }
+
+        override fun getItemCount(): Int = fragments.size
+
+        override fun createFragment(position: Int): Fragment = fragments[position]
+    }
+
+    //    //지도뷰 객체 얻기
     override fun onMapReady(p0: GoogleMap) {
         googleMap = p0
 
@@ -71,4 +113,5 @@ class HomeActivity : BaseActivity() , OnMapReadyCallback {
     override fun getLayoutResId(): Int {
         return R.layout.activity_home
     }
+
 }
